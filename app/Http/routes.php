2855d9d939a330c848e89cd50ee8606ba0c0d2b3
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2015-04-21 06:23:42 +0700

    transaksi otomatis

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -45,3 +45,24 @@ Route::get('/list-stock-chunk', function() {
 	});
 	echo 'Total memory usage : ' . (memory_get_usage() - $begin);
 });
+
+Route::get('/order-product', function() {
+	// memulai transaksi
+	DB::transaction(function()
+	{
+		// Membuat record di table `orders`
+		$order_id = DB::table('orders')->insertGetId(['customer_id'=>1]);
+		// Menambah record baru di `order_products`
+		DB::table('orders_products')->insert(['order_id'=>$order_id, 'product_id'=>5]);
+		// Membayar order (mengisi field `paid_at` di table `orders`)
+		DB::table('orders')->where('id',$order_id)->update(['paid_at'=>new DateTime]);
+
+		// Terjadi error...
+		throw new Exception("Oooppssss.. ada error!");
+
+		// Mengurangi stock product
+		DB::table('products')->decrement('stock');
+	});
+	echo "Berhasil menjual " . DB::table('products')->find(5)->name . '. <br>';
+	echo "Stock terkini : " . DB::table('products')->find(5)->stock;
+});
