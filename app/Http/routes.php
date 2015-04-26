86db789b093108559525270cabfb9ebed05c0beb
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2015-04-26 10:42:45 +0700

    database cache

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -48,21 +48,60 @@ Route::get('/list-stock-chunk', function() {
 
 Route::get('/order-product', function() {
 	// memulai transaksi
-	DB::transaction(function()
-	{
+	DB::beginTransaction();
+
+	try {
 		// Membuat record di table `orders`
 		$order_id = DB::table('orders')->insertGetId(['customer_id'=>1]);
 		// Menambah record baru di `order_products`
 		DB::table('orders_products')->insert(['order_id'=>$order_id, 'product_id'=>5]);
+	} catch (Exception $e) {
+		DB::commit();
+		return "Error: " . $e->getMessage();
+	}
+
+	try {
 		// Membayar order (mengisi field `paid_at` di table `orders`)
 		DB::table('orders')->where('id',$order_id)->update(['paid_at'=>new DateTime]);
-
-		// Terjadi error...
-		throw new Exception("Oooppssss.. ada error!");
-
+		throw new Exception("Server Database mati..");
 		// Mengurangi stock product
 		DB::table('products')->decrement('stock');
-	});
+	} catch (Exception $e) {
+		DB::rollback();
+		return "Error: " . $e->getMessage();
+	}
+
+	// meyimpan transaksi
+	DB::commit();
+
 	echo "Berhasil menjual " . DB::table('products')->find(5)->name . '. <br>';
 	echo "Stock terkini : " . DB::table('products')->find(5)->stock;
+});
+
+Route::get('/customers', function() {
+	DB::connection()->enableQueryLog();
+
+	$products = DB::table('products')->get();
+	$products = DB::table('customers')->whereIn('id', [1,4,5])->select(['name', 'phone'])->get();
+	$customers = DB::table('customers')->leftJoin('membership_types','customers.membership_type_id', '=', 'membership_types.id')->get();
+
+	var_dump(DB::getQueryLog());
+});
+
+Route::get('/product', function() {
+	DB::connection()->enableQueryLog();
+
+	DB::table('products')->where('id', 1)->update(['price'=>110000000]);
+	Cache::forget('product.lowest-price');
+
+	$product = Cache::remember('product.lowest-price', 10, function()
+	{
+	    return DB::table('products')
+		->where('price', DB::table('products')->min('price'))
+		->get();
+	});
+
+	var_dump($product);
+
+	var_dump(DB::getQueryLog());
 });
